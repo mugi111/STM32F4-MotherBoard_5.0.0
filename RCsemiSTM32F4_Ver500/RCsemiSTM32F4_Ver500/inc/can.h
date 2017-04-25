a496d9e5f29b93804cd078e052356b1517abe0f5
mugi111 <takumi5349pino9355@gmail.com>
2017-04-25 19:01:29 +0900

    add can class

---
+++ RCsemiSTM32F4_Ver500/RCsemiSTM32F4_Ver500/inc/can.h
@@ -0,0 +1,23 @@
+/*
+ * can.h
+ *
+ *  Created on: 2017/04/25
+ *      Author: mugi111
+ */
+
+#ifndef CAN_H_
+#define CAN_H_
+
+namespace RCsemi{
+
+class can{
+
+public:
+
+};
+
+}
+
+
+
+#endif /* CAN_H_ */

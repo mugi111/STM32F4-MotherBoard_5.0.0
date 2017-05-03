e93c35c6cb49d9a14e80b3722178220979ec1e04
mugi111 <takumi5349pino9355@gmail.com>
2017-05-03 11:14:57 +0900

    motor driveの中身追加した

--- RCsemiSTM32F4_Ver500/RCsemiSTM32F4_Ver500/src/motor.cpp
+++ RCsemiSTM32F4_Ver500/RCsemiSTM32F4_Ver500/src/motor.cpp
@@ -7,6 +7,13 @@
 
 #include "../inc/motor.h"
 
+#define DUTY_TO_DIR(duty) ((duty) == BRAKEVALUE	? 0x03 : \
+						   (duty) < 0			? 0x02 : \
+						   (duty) > 0	 		? 0x01 : 0x00)
+
+#define ABS_VAL(val) ((val) < 0 ? -(val) : (val))
+
+#define IS_SHORTCURRENT_PATTERN(dir_now, dir_old)	((dir_now) != (dir_old) && (dir_now) && (dir_old))
 
 namespace RCsemi{
 
@@ -22,45 +29,45 @@ motor::motor(unsigned short ch)
 
 int motor::drive(int duty)
 {
-//	u8 port = ch>>2;	// 0:0-3 / 1:4-7
-//	ch &= 0x02; 		// 0-2Ç…êßå¿
-//
-//	u8 dir_now = DUTY_TO_DIR(duty);
-//	u8 dir_old = gMotor_status[port].dir_old[ch];
-//
-//	if(gMotor_status[port].isEnable[ch])	gMotor_status[port].duty[ch] = ABS_VAL(duty);
-//	else									gMotor_status[port].duty[ch] = 0;
-//
-//	/****************************	ä—í ìdó¨ëŒçÙ	***********************************/
-//	if(gMotor_status[port].deadtime[ch])
-//	{
-//		if(IS_SHORTCURRENT_PATTERN(dir_now, dir_old))
-//		{
-//			gMotor_status[port].dir_save[ch] = dir_now;
-//		}
-//		else
-//		{
-//			gMotor_status[port].deadtime[ch] = 0;
-//			gMotor_status[port].dir		[ch] = dir_now;
-//			gMotor_status[port].dir_old [ch] = dir_now;
-//		}
-//	}
-//	else
-//	{
-//		if(IS_SHORTCURRENT_PATTERN(dir_now, dir_old))
-//		{
-//			gMotor_status[port].deadtime[ch] = MOTOR_DEADTIME_10US;
-//			gMotor_status[port].dir		[ch] = 0;
-//			gMotor_status[port].dir_save[ch] = dir_now;
-//		}
-//		else
-//		{
-//			gMotor_status[port].dir	   [ch] = dir_now;
-//			gMotor_status[port].dir_old[ch] = dir_now;
-//		}
-//	}
-//	/******************************************************************************/
-//
+	uint8_t port = m_ch>>2;	// 0:0-3 / 1:4-7
+	m_ch &= 0x02; 		// 0-2Ç…êßå¿
+
+	uint8_t dir_now = DUTY_TO_DIR(duty);
+	uint8_t dir_old = gMotor_status[port].dir_old[m_ch];
+
+	if(gMotor_status[port].isEnable[m_ch])	gMotor_status[port].duty[m_ch] = ABS_VAL(duty);
+	else									gMotor_status[port].duty[m_ch] = 0;
+
+	/****************************	ä—í ìdó¨ëŒçÙ	***********************************/
+	if(gMotor_status[port].deadtime[m_ch])
+	{
+		if(IS_SHORTCURRENT_PATTERN(dir_now, dir_old))
+		{
+			gMotor_status[port].dir_save[m_ch] = dir_now;
+		}
+		else
+		{
+			gMotor_status[port].deadtime[m_ch] = 0;
+			gMotor_status[port].dir		[m_ch] = dir_now;
+			gMotor_status[port].dir_old [m_ch] = dir_now;
+		}
+	}
+	else
+	{
+		if(IS_SHORTCURRENT_PATTERN(dir_now, dir_old))
+		{
+			gMotor_status[port].deadtime[m_ch] = MOTOR_DEADTIME_10US;
+			gMotor_status[port].dir		[m_ch] = 0;
+			gMotor_status[port].dir_save[m_ch] = dir_now;
+		}
+		else
+		{
+			gMotor_status[port].dir	   [m_ch] = dir_now;
+			gMotor_status[port].dir_old[m_ch] = dir_now;
+		}
+	}
+	/******************************************************************************/
+
 
 	m_currentDuty = duty;
 	return duty;

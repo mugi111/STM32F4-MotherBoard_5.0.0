03d77558f47276fd028314ce69943588ca197e48
mugi111 <takumi5349pino9355@gmail.com>
2017-05-03 10:16:53 +0900

    インデント直した

--- RCsemiSTM32F4_Ver500/RCsemiSTM32F4_Ver500/src/rotary_encoder.cpp
+++ RCsemiSTM32F4_Ver500/RCsemiSTM32F4_Ver500/src/rotary_encoder.cpp
@@ -43,10 +43,10 @@ namespace RCsemi{
 
 			TIM_HandleTypeDef htim3;
 			htim3.Instance = TIM3;
-			  htim3.Init.Prescaler = 0;
-			  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-			  htim3.Init.Period = 0;
-			  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+			htim3.Init.Prescaler = 0;
+			htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+			htim3.Init.Period = 0;
+			htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 
 			HAL_TIM_Encoder_Init(&htim3, &Encoder_InitStructure);
 			HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
